7dcd68285e56848eddf9de0eb104ff1233927f54
César Richard <cesar.richard@weezevent.com>
2021-11-09 10:55:50 +0100

    Updated user admin list

--- gesasso/api/admin.py
+++ gesasso/api/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.admin.models import LogEntry
 
 from gesasso.api.models import Action, ActionType, Task, TaskType, Request, Asso, User
+from django.utils.translation import gettext_lazy as _
 
 
 @admin.register(LogEntry)
@@ -84,15 +85,73 @@ class TaskTypeInline(admin.StackedInline):
 
 @admin.register(Action)
 class ActionAdmin(admin.ModelAdmin):
-    def get_form(self, request, obj=None, change=False, **kwargs):
-        form = super().get_form(request, obj, **kwargs)
-        form.base_fields["type"].label_from_instance = lambda o: o.name
-        return form
+    pass
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    pass
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_superuser",
+    )
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
+    search_fields = ("username", "first_name", "last_name", "email")
+    ordering = ("username",)
+    filter_horizontal = (
+        "groups",
+        "user_permissions",
+    )
+
+    def get_fieldsets(self, request, obj=None):
+        if not obj:
+            return self.add_fieldsets
+        return super().get_fieldsets(request, obj)
+
+    def get_form(self, request, obj=None, **kwargs):
+        """
+        Use special form during user creation
+        """
+        defaults = {}
+        if obj is None:
+            defaults["form"] = self.add_form
+        defaults.update(kwargs)
+        return super().get_form(request, obj, **defaults)
+
+    def lookup_allowed(self, lookup, value):
+        # Don't allow lookups involving passwords.
+        return not lookup.startswith("password") and super().lookup_allowed(
+            lookup, value
+        )
 
 
 admin.site.site_header = "Gesasso MK_Ultra"
