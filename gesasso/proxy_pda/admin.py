e976dd93e972dc1467310ab88b9049e4e3affe84
César Richard <cesar.richard2@gmail.com>
2022-02-10 21:09:37 +0100

    Req update

--- gesasso/proxy_pda/admin.py
+++ gesasso/proxy_pda/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 
 from gesasso.proxy_pda.models import Asso
 from gesasso.proxy_pda.utils import sync_assos
@@ -12,6 +14,26 @@ class AssoAdmin(admin.ModelAdmin):
     def get_urls(self):
         urls = super().get_urls()
         my_urls = [
-            path("sync/", sync_assos),
+            path("sync/", self.sync),
         ]
         return my_urls + urls
+
+    @csrf_exempt
+    def sync(self, request):
+        sync_assos(request)
+        self.message_user(request, "All assos are now in sync")
+        return HttpResponseRedirect("../")
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    list_display = ("login", "shortname", "name", "parent")
+    list_filter = (("parent", admin.RelatedOnlyFieldListFilter),)
+    search_fields = ("login", "shortname", "name")
+    ordering = ("login", "shortname", "name")
