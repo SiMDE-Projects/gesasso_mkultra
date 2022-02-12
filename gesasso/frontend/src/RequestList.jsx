43c5a7d1443196d71b046ca3812a1adfb6f712b8
César Richard <cesar.richard2@gmail.com>
2022-02-12 20:15:35 +0100

    Simplified RequestList front component

--- gesasso/frontend/src/RequestList.jsx
+++ gesasso/frontend/src/RequestList.jsx
@@ -31,33 +31,30 @@ const RequestList = function () {
   };
 
   return (
-    <>
-      <p>Mes demandes</p>
-      <Table>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>Status</Table.HeaderCell>
-            <Table.HeaderCell>Title</Table.HeaderCell>
-            <Table.HeaderCell>Due date</Table.HeaderCell>
+    <Table>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell>Status</Table.HeaderCell>
+          <Table.HeaderCell>Title</Table.HeaderCell>
+          <Table.HeaderCell>Due date</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {requests.map((x) => (
+          <Table.Row key={x.id}>
+            <Table.Cell>
+              {translateStatus(x.status)}
+            </Table.Cell>
+            <Table.Cell>
+              {x.title}
+            </Table.Cell>
+            <Table.Cell>
+              {x.due_date}
+            </Table.Cell>
           </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {requests.map((x) => (
-            <Table.Row key={x.id}>
-              <Table.Cell>
-                {translateStatus(x.status)}
-              </Table.Cell>
-              <Table.Cell>
-                {x.title}
-              </Table.Cell>
-              <Table.Cell>
-                {x.due_date}
-              </Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table>
-    </>
+        ))}
+      </Table.Body>
+    </Table>
   );
 };
 
