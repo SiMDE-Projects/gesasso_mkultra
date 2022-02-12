df92d6981cdff39c57af469e21f5a29b44bbfbd0
César Richard <cesar.richard2@gmail.com>
2022-02-12 13:51:19 +0100

    Splitted factories in module

---
+++ gesasso/api/factories/__init__.py
@@ -0,0 +1,9 @@
+from .AssoFactory import AssoFactory
+from .RequestFactory import RequestFactory
+from .UserFactory import UserFactory
+
+__all__ = [
+    AssoFactory,
+    RequestFactory,
+    UserFactory,
+]
