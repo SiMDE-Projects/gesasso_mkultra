b7ffbf83c6c058ddd5261d08acdded69e41eaa3b
César Richard <cesar.richard2@gmail.com>
2022-02-17 13:43:59 +0100

    Added frontend components (AssoSelector, basic UI, RequestList, ...)

---
+++ gesasso/frontend/src/SessionContext.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const SessionContext = React.createContext({
+  user: null,
+  updateUser: () => {
+    // eslint-disable-next-line no-console
+    console.error('updateUser not implemented');
+  },
+});
+export default SessionContext;
