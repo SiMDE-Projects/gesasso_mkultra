b74ef347e64edd69f963352fa7d45b2f310bb7f7
César Richard <cesar.richard2@gmail.com>
2022-03-31 20:23:12 +0200

    Updated env deps

--- gesasso/components/statics.py
+++ gesasso/components/statics.py
@@ -1,7 +1,14 @@
 import os
 from pathlib import Path
 
+import environ
+
+env = environ.Env(
+    BASE_URL=(str, "/"),
+)
+environ.Env.read_env()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_URL = "/static/"
+STATIC_URL = env("BASE_URL") + "static/"
 STATIC_ROOT = "/static"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "gesasso", "frontend", "dist"),)
