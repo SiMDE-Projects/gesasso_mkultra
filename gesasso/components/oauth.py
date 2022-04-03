5455aac634ceaf7898bc4ea8a5029b2b6ca4481c
César Richard <cesar.richard@weezevent.com>
2022-04-03 14:35:47 +0200

    Added GESASSO_OAUTH_REDIRECT_URI env

--- gesasso/components/oauth.py
+++ gesasso/components/oauth.py
@@ -6,6 +6,7 @@ import environ
 env = environ.Env(
     GESASSO_OAUTH_SETTINGS_ID=(str, ""),
     GESASSO_OAUTH_SETTINGS_SECRET=(str, ""),
+    GESASSO_OAUTH_REDIRECT_URI=(str, "http://localhost:8003/oauth/callback"),
 )
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -17,7 +18,7 @@ OAUTH_SETTINGS = {
     "client_secret": env("GESASSO_OAUTH_SETTINGS_SECRET"),
     "authorization_url": "https://assos.utc.fr/oauth/authorize",
     "token_url": "https://assos.utc.fr/oauth/token",
-    "redirect_uri": "http://localhost:8003/oauth/callback",
+    "redirect_uri": env("GESASSO_OAUTH_REDIRECT_URI"),
     "scopes": ["user-get-info", "user-get-roles", "user-get-assos"],
     "api_base_url": "https://assos.utc.fr/api/v1",
 }
