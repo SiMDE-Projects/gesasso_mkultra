55c4f37c66866d696e8792fe7422a2e212624dcd
César Richard <cesar.richard2@gmail.com>
2022-04-03 14:12:12 +0200

    Changed env usage

--- gesasso/components/statics.py
+++ gesasso/components/statics.py
@@ -2,16 +2,16 @@ import os
 
 import environ
 
+# Set the project base directory
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 env = environ.Env(
     GESASSO_BASE_URL=(str, "/"),
     GESASSO_STATIC_URL=(str, "/static/"),
-    GESASSO_STATIC_ROOT=(str, "/static/"),
+    GESASSO_STATIC_ROOT=(str, os.path.join(BASE_DIR, "static")),
 )
 
-# Set the project base directory
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Take environment variables from .env file
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
-STATIC_URL = env("STATIC_URL")
-STATIC_ROOT = env("STATIC_ROOT")
+STATIC_URL = env("GESASSO_STATIC_URL", env("GESASSO_BASE_URL") + "static/")
+STATIC_ROOT = env("GESASSO_STATIC_ROOT")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "gesasso", "frontend", "dist"),)
