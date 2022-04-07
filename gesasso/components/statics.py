f34fe3730b1d0e049df2827a74d40c345c65a11b
César Richard <cesar.richard2@gmail.com>
2022-04-07 03:09:31 +0200

    Fixed static url

--- gesasso/components/statics.py
+++ gesasso/components/statics.py
@@ -6,12 +6,12 @@ import environ
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 env = environ.Env(
     GESASSO_BASE_URL=(str, "/"),
-    GESASSO_STATIC_URL=(str, "/static/"),
+    GESASSO_STATIC_URL=(str, "static/"),
     GESASSO_STATIC_ROOT=(str, os.path.join(BASE_DIR, "static")),
 )
 
 # Take environment variables from .env file
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
-STATIC_URL = env("GESASSO_STATIC_URL", env("GESASSO_BASE_URL") + "static/")
+STATIC_URL = env("GESASSO_BASE_URL") + env("GESASSO_STATIC_URL", "static/")
 STATIC_ROOT = env("GESASSO_STATIC_ROOT")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "gesasso", "frontend", "dist"),)
